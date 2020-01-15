3dc33331a852f5addc1487abed08d2f503b43240
spettigrew <sara.pettigrew5@gmail.com>
2020-01-15 12:15:49 -0700

    Import routers

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 express = require('express')
+const helmet = require("helmet")
+const cors = require("cors")
 
 // import routers
-const userRouter = require('./users/user-router')
+const authRouter = require("./auth/auth-router")
+const userRouter = require("./users/user-router")
+
 
 require('dotenv').config()
 
 const server = express();
-
+server.use(helmet())
+server.use(cors())
 server.use(express.json());
 
 // import api endpoints, router files.
 server.use('/api/users', userRouter);
+server.use('/api/auth', authRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
