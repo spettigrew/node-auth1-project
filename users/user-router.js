dfece2e669dff28262abc0838eccfbde6cf7d11b
spettigrew <sara.pettigrew5@gmail.com>
2020-01-15 11:33:49 -0700

    User-router setup

---
+++ users/user-router.js
@@ -0,0 +1,48 @@
+const bycrypt = require("bcryptjs")
+const express = require("express")
+const userModel = require("./user.model")
+
+const router = express.Router()
+
+
+function restricted() {
+    const authError = {
+        message: "Invalid credentials."
+    }
+
+    return async (req, res, next) => {
+        try {
+            const { username, password } = req.headers
+            if (!username || !password) {
+                return res.status(401).json(authError)
+            }
+
+            const user = await userModel.findBy({ username })
+            .first()
+            if (!user) {
+                return res.status(401).json(authError)
+            }
+
+            const passwordValid = await bcrypt.compare(password, user.password)
+            if (!passwordValid) {
+                return res.status(401).json(authError)
+            }
+
+            next()
+        } catch (err) {
+            next(err)
+        }
+    }
+}
+
+router.get("/", restricted(), async (req, res, next) => {
+    try {
+        const users = await userModel.find()
+
+        res.json(users)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
