8e7c3c0e3cbce6f815961ca272c220dcf8ea3e75
spettigrew <sara.pettigrew5@gmail.com>
2020-01-15 14:28:14 -0700

    Routers moved and .get added

--- server.js
+++ server.js
@@ -2,21 +2,26 @@ express = require('express')
 const helmet = require("helmet")
 const cors = require("cors")
 
-// import routers
-const authRouter = require("./auth/auth-router")
-const userRouter = require("./users/user-router")
-
-
-require('dotenv').config()
-
 const server = express();
 server.use(helmet())
 server.use(cors())
 server.use(express.json());
 
+require('dotenv').config()
+
+// import routers
+const authRouter = require("./auth/auth-router")
+const userRouter = require("./users/user-router")
+
 // import api endpoints, router files.
-server.use('/api/users', userRouter);
-server.use('/api/auth', authRouter)
+server.use('/users', userRouter);
+server.use('/auth', authRouter)
+
+server.get("/", (req, res, next) => {
+    res.json({
+        message: "Welcome to our API",
+    })
+})
 
 server.use((err, req, res, next) => {
     console.log(err)
