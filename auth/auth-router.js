b4c460c15f22dbe1272da5df7684b9f653a1e9c0
spettigrew <sara.pettigrew5@gmail.com>
2020-01-15 12:16:20 -0700

    Auth router setup

---
+++ auth/auth-router.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs")
+const express = require("express")
+const userModel = require("../users/user.model")
+
+const router = express.Router()
+
+router.post("/register", async (req, res, next) => {
+    try {
+        const savedUser = await userModel.add(req.body)
+        res.status(201).json(savedUser)
+    }catch (err) {
+        next(err)
+    }
+})
+
+router.post("/login", async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const user = await userModel.findBy({ username })
+        .first()
+        const passwordValid = await bcrypt.compare(password, user.password)
+
+        if (user && passwordValid) {
+            res.status(200).json({ message: `Welcome, ${user.username}!`, })
+        } else {
+            res.status(401).json({ message: "Invalid credentials.", })
+        }
+    }   catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
